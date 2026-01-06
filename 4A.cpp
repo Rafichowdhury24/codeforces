c5de6fb3e43f1c1c0ba661d76a7fac6a6192293b
Rafichowdhury24 <ar370442@gmail.com>
2026-01-06 22:58:53 +0600

    Add files via upload

---
+++ 4A.cpp
@@ -0,0 +1,11 @@
+#include<iostream>      //Rafimia242    5/11/2025
+using namespace std;
+int main()
+{
+    int w;
+    cin>> w;
+    if(w%2==0 && w>2){
+        cout<< "Yes";}
+    else
+    cout<< "NO";
+}
