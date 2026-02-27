85ea7c92a3a68e4d4ef24fa9e20ee8e7a71293f3
Rafi Chowdhury <ar370442@gmail.com>
2026-02-27 19:45:13 +0600

    Add files via upload

---
+++ 282A.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>  //Rafimia69     5/1/2025
+using namespace std;
+int main()
+{
+    int n,x=0;
+    string s;
+    cin>>n;
+    while(n--)
+    {   cin>>s;
+        if(s[1]=='+')
+            x++;
+        else
+            x--;
+    }
+    cout<<x<<endl;
+
+}
